bcf13c034791122c5a81d2a34aa2de1cf1f63368
Bruce <brucewang@uqpay.com>
2025-07-18 21:51:54 +0800

    test auto gener

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello, GitHub Actions!",
+			"message": "Hello, GitHub Actions!d,我加了点东西",
 		})
 	})
 	r.Run(":8080")
